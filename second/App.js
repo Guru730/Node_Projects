8ddecc6a413705f824cdfcea787ff90e71d343af
Guru <gurshran4544@gmail.com>
2021-04-10 20:16:27 +0530

    just created the login and registeration page without functionality

--- second/App.js
+++ second/App.js
@@ -1,25 +1,30 @@
 const express = require('express');
-
 const app = express();
+const port = 3003;
+const middleware = require('./middleware')
+const path = require('path')
 
-const port = 3006;
-
-const middleware = require('./middleware');
-
-const server = app.listen(port, () => console.log('server listening on port ' + port));
+const server = app.listen(port, () => console.log("Server listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/",middleware.requireLogin, (req, res, next) => {
-
-        var payload = {
-            pageTitle: "home"
-        };
-        
-     res.status(200).render("home", payload);
-});
+// Routes
+const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
 
 
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
 
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
+
+    var payload = {
+        pageTitle: "Home"
+    }
+
+    res.status(200).render("home", payload);
+})
